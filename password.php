8a78004ee3ff6a1dd59bf95d25979272324d2155
Nicholas Clark <clarkni5@gmail.com>
2012-05-06 12:24:27 -0700

    Adding file.

---
+++ password.php
@@ -0,0 +1,28 @@
+<?php
+
+	/**
+	 * Generated a random pronounceable password.
+	 *
+	 * @param int $length The length of the generated password.
+	 *
+	 * @return A pronounceable password.
+	 */
+	function generate_password($length = 8) {
+		$vowels = array("a","e","i","o","u");
+		$consonants = array("b","c","d","g","h","j","k","l","m","n","p","r","s","t",
+			"u","v","w","tr","cr","br","fr","th","dr","ch","ph","wr","st","sp",
+			"sw","pr","sl","cl");
+
+		$num_vowels = count($vowels);
+		$num_consonants = count($consonants);
+
+		$password = '';
+		for($i = 0; $i < $length; $i++){
+			$password .= $consonants[rand(0, $num_consonants - 1)] . $vowels[rand(0, $num_vowels - 1)];
+		}
+		$password = substr($password, 0, $length);
+
+		return $password;
+	}
+	
+	echo generate_password();
